21934a1e297250b3c8470d6cc03d6f32fc93ffb3
harshyadav02 <hy043768@gmail.com>
2023-06-15 21:11:29 +0530

    Primitive datatypes

---
+++ JavaScript/5.PrimitiveData.js
@@ -0,0 +1,19 @@
+//Numbers
+var a = 10;
+var f = 1.2
+
+//String
+var s =  "Harsh Yadav";
+
+//Boolean
+var  bool1 = true;
+var  bool2 = false;
+
+//Undefind
+var z;
+
+//Null
+var n = null 
+
+console.log(a,f,s,bool1,bool2,z,n);
+
