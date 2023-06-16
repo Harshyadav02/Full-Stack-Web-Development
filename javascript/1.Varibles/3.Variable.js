cd205004ab0b7f7b16e304288ba17bfee7a9d750
harshyadav02 <hy043768@gmail.com>
2023-06-16 21:30:33 +0530

    Types of variables

---
+++ javascript/1.Varibles/3.Variable.js
@@ -0,0 +1,18 @@
+// By let Keyword
+let a = 5;
+let b = "Harsh Yadav";
+let d = 5.5;
+
+console.log(a, b, d);
+
+// By Var keyword 
+var f = 10;
+var f = 20;
+console.log(f); 
+
+// Rules to declare variables 
+    // 1) cannot be a reserved word 
+    // 2) Should be meaningfull
+    // 3) Shouldn't Start with Number
+    // 4) cnnot contain space or hypen
+    // 5) Use camelCase format 
