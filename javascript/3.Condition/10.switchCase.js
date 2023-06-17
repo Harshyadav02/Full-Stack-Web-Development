2d21569a452d46177e9507bba675491313560ee7
harshyadav02 <hy043768@gmail.com>
2023-06-17 19:02:38 +0530

    switch in javascript

---
+++ javascript/3.Condition/10.switchCase.js
@@ -0,0 +1,14 @@
+let num = 3;
+switch(num){
+    case 1 : console.log('A');
+    break
+    
+    case 2 : console.log('B');
+    break
+
+    case 3 : console.log('C');
+    break
+ 
+    default: console.log('D')
+    
+}  
