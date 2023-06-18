406cfa27ec844619d718a9b89e4edab79db47db1
harshyadav02 <hy043768@gmail.com>
2023-06-18 22:21:04 +0530

    ForLoop

---
+++ javascript/4.Loops/11.ForLoop.js
@@ -0,0 +1,12 @@
+// Loops are used for the repeataion of the task there are several loops one them is for loop , for loop contain three things :- Intialization , condition , updation 
+
+// In the below example var i = 0 is intialitsion , i <5 is termination condition anf i++ is updation condition
+for (var i = 0; i < 5; i++){
+    console.log("Harsh")
+} 
+//output 
+    //Harsh
+    // Harsh
+    // Harsh
+    // Harsh
+    // Harsh
