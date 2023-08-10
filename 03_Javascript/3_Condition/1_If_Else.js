8adf51bf148ac2abbfd7f8908ab404e11cdfbd7c
harshyadav02 <hy043768@gmail.com>
2023-08-10 17:41:49 +0530

    If else

---
+++ 03_Javascript/3_Condition/1_If_Else.js
@@ -0,0 +1,10 @@
+marks = 21;
+if (marks >20){
+    console.log('Marks are greater then 20');
+}
+else if (marks < 20) {
+    console.log("Marks are less then 20");
+}
+else{
+    console.log("Marks are equla to 20")
+}
