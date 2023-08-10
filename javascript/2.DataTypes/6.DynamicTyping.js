e27a527096dc46ec5470a2ce5f0d0cd9c9b8a748
Harsh Yadav <91467622+Harshyadav02@users.noreply.github.com>
2023-08-10 17:51:15 +0530

    Delete javascript directory

--- javascript/2.DataTypes/6.DynamicTyping.js
+++
@@ -1,7 +0,0 @@
-// Value can be changed after decalring a varible lke python but in other languages like java or c++ or c if we decaled a variable with its datatype then we will not be able to change the value 
-
-let a = 20;
-console.log(a)
-a = 'harsh'
-
-console.log(a)
--- javascript/4.Loops/13.DoWhile.js
+++
@@ -1,5 +0,0 @@
-var i = 1;
-do{
-    console.log("Harsh")
-    i++
-}while(i<=5);
